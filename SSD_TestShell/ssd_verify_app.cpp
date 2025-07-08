1b48d953c005f6ec73cbb299412a7b824dd0687a
sseonnn <com2769@gmail.com>
2025-07-08 14:43:25 +0900

    [Feature] Add ssd_verify_app

---
+++ SSD_TestShell/ssd_verify_app.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include "ssd_verify_app.h"
+#include "parser/parser.h"
+#include "command_processer.h"
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+void SsdVerifyApp::startVerify() {
+	Parser parser;
+	vector<string> parsedInput = parser.parse(inputCmd);
+
+	CommandProcesser commandProcesser;
+	commandProcesser.run(parsedInput);
+}
+
+void SsdVerifyApp::setInputCmd(string input) {
+	inputCmd = input;
+}
+
+void SsdVerifyApp::getUserCmdLine() {
+	cout << "Shell> ";
+	string input;
+	getline(cin, input);
+	setInputCmd(input);
+}
