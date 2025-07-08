07b20592f4bb2e79dea28b7d87264887279fca72
tdjung <td.jung@samsung.com>
2025-07-08 10:58:43 +0900

    [feature] checker interface & mock init upload

--- SSD_TestShell/command_mock.h
+++ SSD_TestShell/command_mock.h
@@ -7,3 +7,10 @@ public:
 	MOCK_METHOD(void, set, (const string cmd), (override));
 	MOCK_METHOD(bool, run, (), (override));
 };
+
+class CheckerMock : public Checker{
+public:
+	MOCK_METHOD(bool, nandWriteAndReadAndChecker, (int, const string&), (override));
+	MOCK_METHOD(bool, nandReadAndChecker, (int, const string&), (override));
+	MOCK_METHOD(bool, nandOutputChecker, (string), (override));
+};
--- SSD_TestShell/shell_command.h
+++ SSD_TestShell/shell_command.h
@@ -8,3 +8,10 @@ public:
 	virtual void set(const string cmd) = 0;
 	virtual bool run() = 0;
 };
+
+class Checker {
+public:
+	virtual bool nandWriteAndReadAndChecker(int LBA, const string& data) = 0;
+	virtual bool nandReadAndChecker(int LBA, const string& data) = 0;
+	virtual bool nandOutputChecker(string data) = 0;
+};
