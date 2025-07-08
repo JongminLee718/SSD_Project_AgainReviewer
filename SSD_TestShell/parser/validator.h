29469a49ca3d30eb99ffddfac3a9a9c69870f864
encorekim <rlawodus95@hanmail.net>
2025-07-08 15:55:36 +0900

    [refactor] Introduce ValidatorFactory for Parser
    - Add Validator abstraction class
    - Add Validator child classes for each command
    - Introduce Simple Factory for validators

---
+++ SSD_TestShell/parser/validator.h
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include "validator_utils.h"
+
+class Validator {
+public:
+	Validator() = default;
+	~Validator() = default;
+
+	virtual void validate(const std::vector<std::string> token) = 0;
+};
+
+class ReadValidator : public Validator {
+public:
+	void validate(const std::vector<std::string> tokens) override {
+		ValidatorUtils::checkTokenCount(tokens, EXPECTED_NUM_TOKENS);
+		ValidatorUtils::validateAddress(tokens[1]);
+	}
+private:
+	const size_t EXPECTED_NUM_TOKENS = 2;
+};
+
+class WriteValidator : public Validator {
+public:
+	void validate(const std::vector<std::string> tokens) override {
+		ValidatorUtils::checkTokenCount(tokens, EXPECTED_NUM_TOKENS);
+		ValidatorUtils::validateAddress(tokens[1]);
+		ValidatorUtils::validateDataValue(tokens[2]);
+	}
+private:
+	const size_t EXPECTED_NUM_TOKENS = 3;
+};
+
+class FullWriteValidator : public Validator {
+public:
+	void validate(const std::vector<std::string> tokens) override {
+		ValidatorUtils::checkTokenCount(tokens, EXPECTED_NUM_TOKENS);
+		ValidatorUtils::validateDataValue(tokens[1]);
+	}
+private:
+	const size_t EXPECTED_NUM_TOKENS = 2;
+};
+
+class SimpleValidator : public Validator {
+public:
+	void validate(const std::vector<std::string> tokens) override {
+		ValidatorUtils::checkTokenCount(tokens, EXPECTED_NUM_TOKENS);
+	}
+private:
+	const size_t EXPECTED_NUM_TOKENS = 1;
+};
