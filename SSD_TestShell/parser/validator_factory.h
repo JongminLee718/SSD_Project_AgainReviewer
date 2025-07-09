3149d2a9c1ec8be76419f14c609b43ecb54fef90
encorekim <rlawodus95@hanmail.net>
2025-07-09 09:46:57 +0900

    [fix] Fix validator to just 1_, instead of startwith 1_

--- SSD_TestShell/parser/validator_factory.h
+++ SSD_TestShell/parser/validator_factory.h
@@ -21,23 +21,11 @@ public:
 		if (command == "fullread" || command == "exit" || command == "help") {
 			return std::make_unique<SimpleValidator>();
 		}
-		if (startsWith(command, "1_") || startsWith(command, "2_") || startsWith(command, "3_")) {
+		if (command == "1_" || command == "2_" || command == "3_" ||
+			command == "1_FullWriteAndReadCompare" || command == "2_PartialLBAWrite" || command == "3_WriteReadAging") {
 			return std::make_unique<ScriptValidator>();
 		}
 
 		throw std::invalid_argument("INVALID COMMAND");
 	}
-
-private:
-	static bool startsWith(const std::string& str, const std::string& prefix) {
-		return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
-	}
-
-	const std::unordered_set<std::string> simpleCommands = {
-	"fullread", "exit", "help" };
-
-	const std::unordered_set<std::string> scriptCommands = {
-		"1_FullWriteAndReadCompare", "2_PartialLBAWrite", "3_WriteReadAging" };
-
-
 };
