32662785d3b47e99a90fbd967194449b8f1e14ca
encorekim <rlawodus95@hanmail.net>
2025-07-09 17:01:20 +0900

    [refactor] add fixture to logger test
    but it is still long...

--- SSD_TestShell/test/logger_test.cpp
+++ SSD_TestShell/test/logger_test.cpp
@@ -8,91 +8,78 @@
 using std::string;
 using namespace testing;
 
-TEST(LoggerTest, PrintLogLine) {
+class LoggerFixture : public Test {
+public:
 	Logger logger;
 	const std::string logFile = logger.getLogFile();
 
+	string currentPattern;
+	string minuteAfterPattern;
+
+private:
+	void SetUp() override {
+		std::time_t now = std::time(nullptr);
+		string currentTimestamp = getTimeStamp(now);
+		string minuteAfterTimestamp = getTimeStamp(now - 60);
+
+		currentPattern = getLineWithTimeStamp(currentTimestamp);
+		minuteAfterPattern = getLineWithTimeStamp(minuteAfterTimestamp);
+	}
+
+	string getTimeStamp(std::time_t now) {
+		std::tm tm;
+		localtime_s(&tm, &now);
+		char buf[20];
+		std::strftime(buf, sizeof(buf), "%y.%m.%d %H:%M", &tm);
+		return string(buf);
+	}
+
+	string getLineWithTimeStamp(const string& timestamp) {
+		return R"(\[)" + timestamp + R"(\] Foo\.bar\(\)\s*: some message\.)";
+	}
+};
+
+TEST_F(LoggerFixture, PrintLogLine) {
 	logger.print("Foo.bar()", "some message.");
 
 	std::ifstream in(logFile);
-	std::string line, last;
+	string line, last;
 	while (std::getline(in, line))
 		if (!line.empty()) last = line;
 	in.close();
 
-	std::time_t now = std::time(nullptr);
-	std::tm tm;
-	localtime_s(&tm, &now);
-	char buf[20];
-	std::strftime(buf, sizeof(buf), "%y.%m.%d %H:%M", &tm);
-	std::string timestamp(buf);
-
-	std::string pattern_now =
-		R"(\[)" + timestamp + R"(\] Foo\.bar\(\)\s*: some message\.)";
-	std::string pattern_prev =
-		R"(\[)" + timestamp + R"(\] Foo\.bar\(\)\s*: some message\.)";
-
-	EXPECT_TRUE(std::regex_match(last, std::regex(pattern_now)) ||
-		std::regex_match(last, std::regex(pattern_prev)));
+	EXPECT_TRUE(std::regex_match(last, std::regex(currentPattern)) ||
+		std::regex_match(last, std::regex(minuteAfterPattern)));
 
 	std::filesystem::remove(logFile);
 }
 
-TEST(LoggerTest, PrintMultiLogLine) {
-	Logger logger;
-	const std::string logFile = logger.getLogFile();
-
+TEST_F(LoggerFixture, PrintMultiLogLine) {
 	logger.print("Foo.bar()", "some message.");
-	logger.print("Foo.baz()", "some new message.");
-	logger.print("Foo.qux()", "some newest message.");
+	logger.print("Foo.bar()", "some message.");
+	logger.print("Foo.bar()", "some message.");
 
 	std::ifstream in(logFile);
-	ASSERT_TRUE(in.is_open());
-
 	std::vector<std::string> lines;
-	std::string line;
+	string line;
 	while (std::getline(in, line)) {
 		if (!line.empty())
 			lines.push_back(line);
 	}
 	in.close();
 
-	std::string line1 = lines[lines.size() - 3];
-	std::string line2 = lines[lines.size() - 2];
-	std::string line3 = lines[lines.size() - 1];
+	string line1 = lines[lines.size() - 3];
+	string line2 = lines[lines.size() - 2];
+	string line3 = lines[lines.size() - 1];
 
-	std::time_t now = std::time(nullptr);
+	EXPECT_TRUE(std::regex_match(line1, std::regex(currentPattern)) ||
+		std::regex_match(line1, std::regex(minuteAfterPattern)));
 
-	std::tm tm_now;
-	localtime_s(&tm_now, &now);
-	char buf_now[20];
-	std::strftime(buf_now, sizeof(buf_now), "%y.%m.%d %H:%M", &tm_now);
-	std::string ts_now(buf_now);
+	EXPECT_TRUE(std::regex_match(line2, std::regex(currentPattern)) ||
+		std::regex_match(line2, std::regex(minuteAfterPattern)));
 
-	std::tm tm_prev;
-	now -= 60;
-	localtime_s(&tm_prev, &now);
-	char buf_prev[20];
-	std::strftime(buf_prev, sizeof(buf_prev), "%y.%m.%d %H:%M", &tm_prev);
-	std::string ts_prev(buf_prev);
-
-	std::string pattern1_now = R"(\[)" + ts_now + R"(\] Foo\.bar\(\)\s*: some message\.)";
-	std::string pattern1_prev = R"(\[)" + ts_prev + R"(\] Foo\.bar\(\)\s*: some message\.)";
-
-	std::string pattern2_now = R"(\[)" + ts_now + R"(\] Foo\.baz\(\)\s*: some new message\.)";
-	std::string pattern2_prev = R"(\[)" + ts_prev + R"(\] Foo\.baz\(\)\s*: some new message\.)";
-
-	std::string pattern3_now = R"(\[)" + ts_now + R"(\] Foo\.qux\(\)\s*: some newest message\.)";
-	std::string pattern3_prev = R"(\[)" + ts_prev + R"(\] Foo\.qux\(\)\s*: some newest message\.)";
-
-	EXPECT_TRUE(std::regex_match(line1, std::regex(pattern1_now)) ||
-		std::regex_match(line1, std::regex(pattern1_prev)));
-
-	EXPECT_TRUE(std::regex_match(line2, std::regex(pattern2_now)) ||
-		std::regex_match(line2, std::regex(pattern2_prev)));
-
-	EXPECT_TRUE(std::regex_match(line3, std::regex(pattern3_now)) ||
-		std::regex_match(line3, std::regex(pattern3_prev)));
+	EXPECT_TRUE(std::regex_match(line3, std::regex(currentPattern)) ||
+		std::regex_match(line3, std::regex(minuteAfterPattern)));
 
 	std::filesystem::remove(logFile);
 }
