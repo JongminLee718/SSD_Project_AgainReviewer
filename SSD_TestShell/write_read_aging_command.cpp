95b74b0c11358302223a9eed2a1cfef541822b05
tdjung <td.jung@samsung.com>
2025-07-10 15:22:10 +0900

    [refactoring] test script add PASS/FAIL LOG

--- SSD_TestShell/write_read_aging_command.cpp
+++ SSD_TestShell/write_read_aging_command.cpp
@@ -10,6 +10,7 @@ bool WriteReadAgingCommand::run(vector<string> commands) {
 		ssd->read(to_string(0));
 		bool result = utils->outputChecker(randData);
 		if (!result) {
+			LOG("WriteReadAgingCommand 0 FAIL");
 			cout << "FAIL\n";
 			return false;
 		}
@@ -17,11 +18,13 @@ bool WriteReadAgingCommand::run(vector<string> commands) {
 		ssd->read(to_string(99));
 		result = utils->outputChecker(randData);
 		if (!result) {
+			LOG("WriteReadAgingCommand 99 FAIL");
 			cout << "FAIL\n";
 			return false;
 		}
 	}
 
+	LOG("WriteReadAgingCommand PASS");
 	cout << "PASS\n";
 	return true;
 }
