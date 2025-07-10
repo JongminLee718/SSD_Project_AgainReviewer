8121a41e06c4bd4bc00d73105689ce1d2159f4c3
donghyuck <dheda359@gmail.com>
2025-07-10 15:32:39 +0900

    [refactoring] Extract function

--- SSD_Driver/buffer_manager.h
+++ SSD_Driver/buffer_manager.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 struct Command {
 	std::string type;
@@ -15,9 +18,12 @@ public:
 	BufferManager(std::string nandPath);
 
 	void initializeBuffer();
+	void makeBufferFiles();
 	void loadBufferData();
 	void parsingCommandBuffer(const std::vector<std::string> parts);
 	void updateBufferFile();
+	void renameBuffers(std::string& desired_filename_str, std::vector<std::filesystem::path>& current_paths, int i);
+	void generateFileFormatName(int i, std::string& desired_filename_str);
 	void addCommandInBuffer(const std::string& command_type, int address, int valueOrSize);
 	void updateBufferWithNewCommands(const std::vector<Command>& commands_to_add);
 	bool readFromBuffer(int address, unsigned int& outValue);
